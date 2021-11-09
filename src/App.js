323146c0570c0bdb43576bd127662b81f0cabb0a
9iaashalaby <30384731+9iaashalaby@users.noreply.github.com>
2021-11-08 21:09:07 -0800

    Solving React Component State Challenge Practice

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react"
+
+// Challenge:
+// Given an incomplete class-based component without a constructor, 
+// add a constructor and initialize state to fix the broken component.
+
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: "Sally",
+            age: 13
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.name}</h1>
+                <h3>{this.state.age} years old</h3>
+            </div>
+        );
+    }
+}
